0b7117b9b6c20bd9cca13016c1969d53aaa908c3
Alibi Melis <melis.alibi2000@gmail.com>
2022-04-23 19:07:45 +0600

    Fixed image ratio issue on safari

--- src/Minicart/MinicartItem.js
+++ src/Minicart/MinicartItem.js
@@ -8,7 +8,6 @@ class MinicartItem extends Component {
 
   render() {
     const { item, currency, inc, dec } = this.props;
-    const attribute = item.product.attributes[0];
     const price = findProductPrice(item.product, currency);
     return (
       <div className="minicart-item">
@@ -16,21 +15,23 @@ class MinicartItem extends Component {
           <div>{item.product.brand}</div>
           <div>{item.product.name}</div>
           <div className="price">{priceToString(price)}</div>
-          <ProductAttribute
-            condensed
-            className="attributes"
-            attr={attribute}
-            onSetAttr={this.onSetAttr}
-            selected={item.attributes ? item.attributes[attribute.id] : null}
-            key={attribute.id}
-          />
+          {item.product.attributes.map((attribute) => (
+            <ProductAttribute
+              condensed
+              className="attributes"
+              attr={attribute}
+              onSetAttr={this.onSetAttr}
+              selected={item.attributes ? item.attributes[attribute.id] : null}
+              key={attribute.id}
+            />
+          ))}
         </div>
         <div className="quantity-control">
           <button onClick={inc}>+</button>
           {item.qty}
           <button onClick={dec}>-</button>
         </div>
-        <div>
+        <div className="image">
           <img src={item.product.gallery[0]} alt={item.product.name} />
         </div>
       </div>
